231ddac19d87723f35f81d44211382c9bd52d572
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-04-17 23:25:29 +0800

    Added Login and Logout

--- merchstore/merchstore/urls.py
+++ merchstore/merchstore/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/',include('store.urls',namespace="store")),
+    path('', include('django.contrib.auth.urls')), 
     
 ]
 if settings.DEBUG:
