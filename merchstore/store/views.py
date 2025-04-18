db7e0ca0db46a515007e6df8a5f1aa2337a4625b
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-04-18 17:51:04 +0800

    Created Product Creation View

--- merchstore/store/views.py
+++ merchstore/store/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -32,8 +33,20 @@ def product_detail(request, num=1):
     }
     return render(request,'product_detail.html',ctx)
 
+@login_required
 def product_create(request):
-    pass
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.owner = request.user.profile  # set owner manually
+            product.save()
+            return redirect('store:product_detail', product.id)
+    else:
+        form = ProductForm()
+
+    return render(request, 'product_create.html', {'form': form})
+
 
 def product_update(request):
     pass
